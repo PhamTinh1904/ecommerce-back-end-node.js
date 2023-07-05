68002d25e50c2ae4b4ca16ccbd3f9b19d06b4d77
unknown <phamtinhpy2017@gmail.com>
2023-07-05 11:14:47 +0700

    05/07/2023

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const compression = require("compression");
+const express = require("express");
+const helmet = require('helmet');
+const morgan = require("morgan");
+const app = express();
+
+// init middlewares
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression())
+
+//init routes
+app.get('/', (req, res, next) => {
+    const strCompress = "Hello Factips";
+
+    return res.status(200).json({
+        message: 'Welcome to Factipsjs',
+        metadata: strCompress.repeat(100000)
+    })
+})
+
+module.exports = app;
